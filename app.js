9fc8803a844fcfa90fc79bb0a291717e8cec7f0c
Serkan ISIK <dehnas79@gmail.com>
2022-10-10 11:35:03 +0300

    create tr element

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const mail = document.getElementById('mail');
 
 const form = document.getElementById('form-rehber');
 
+const kisiListesi = document.querySelector('.kisi-listesi')
+
 // event listenerların tanımlanması
 form.addEventListener('submit', e =>{
     e.preventDefault();
@@ -16,7 +18,7 @@ form.addEventListener('submit', e =>{
     
     const kontrol = verileriKontrolEt(eklenecekKisi);
     if(kontrol.durum){
-        console.log("Sorun yok..");
+        kisiyiEkle(eklenecekKisi);
     }else{
         bilgiOluştur(kontrol.mesaj, kontrol.durum);
     }
@@ -25,25 +27,54 @@ form.addEventListener('submit', e =>{
 
 });
 
+// tüm kişiler için dizi
+const tumKisilerDizisi = [];
+
+function kisiyiEkle(object){
+    const olusturulanTrElementi = document.createElement('tr');
+    olusturulanTrElementi.innerHTML = ` 
+        <td>${object.ad}</td>
+        <td>${object.soyad}</td>
+        <td>${object.mail}</td>    
+        <td>
+            <button class="btn btn--edit"><i class="far fa-edit"></i></button>
+            <button class="btn btn--delete"><i class="far fa-trash-alt"></i></button>
+        </td>
+    `;
+
+    kisiListesi.appendChild(olusturulanTrElementi);
+    
+    tumKisilerDizisi.push(object);
+
+    bilgiOluştur('Kisi rehbere kaydedildi.', true);
+    console.log(tumKisilerDizisi);
+}
+
+
 function verileriKontrolEt(kisi){
-    //objelerde in kullanımı
-    for(const key in kisi){
+    //hata kontrolu
+    for(const key in kisi){ //objelerde in kullanımı
         //console.log(kisi[deger]);
         if(kisi[key]){
             //console.log(kisi[key]);
         }else{
             const sonuc = {
                 durum: false,
-                mesaj: 'Boş alan bırakmayınız...'
+                mesaj: 'Boş alan bırakmayınız !'
             }
             return sonuc;
         }
     }
+    //hata yoksa inputları temizle sonra return yap
+    clearInputs();
+    
     return {
         durum: true,
         mesaj: ''
     }
 }
+
+
 
 function bilgiOluştur(mesaj, durum){
     const olusturulanBilgi = document.createElement('div');
@@ -51,13 +82,26 @@ function bilgiOluştur(mesaj, durum){
     olusturulanBilgi.className = 'bilgi';
     
     if(durum){
-        olusturulanBilgi.classList.add('bilgi--succes');
+        olusturulanBilgi.classList.add('bilgi--success');
     }else{
         olusturulanBilgi.classList.add('bilgi--error');
     }
     //olusturulanBilgi.classList.add(durum ? 'bilgi--succes' : 'bilgi--error');
 
-
     document.querySelector('.container').insertBefore(olusturulanBilgi, form);
 
+    // bilgi div' inin silinmesi
+    setTimeout(function(){
+        const silinecekDiv = document.querySelector('.bilgi');
+        if(silinecekDiv){
+            silinecekDiv.remove();
+        }
+    },2000)    
+}
+
+//kayıt 
+function clearInputs() {
+    ad.value = '';
+    soyad.value = '';
+    mail.value = '';
 }
