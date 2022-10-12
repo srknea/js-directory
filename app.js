c1b548c2c8de3c6ebf2d261afc7d87d4aeeee79f
Serkan ISIK <dehnas79@gmail.com>
2022-10-12 12:34:02 +0300

    edit button

--- app.js
+++ app.js
@@ -11,37 +11,76 @@ const kisiListesi = document.querySelector('.kisi-listesi')
 //kaydet
 form.addEventListener('submit', e =>{
     e.preventDefault();
-    const eklenecekKisi = {
+    const kisi = {
         ad: ad.value,
         soyad: soyad.value,
         mail: mail.value 
     }
     
-    const kontrol = verileriKontrolEt(eklenecekKisi);
+    const kontrol = verileriKontrolEt(kisi);
     if(kontrol.durum){
-        kisiyiEkle(eklenecekKisi);
+        if(secilenSatir){
+            //güncelleme yap
+            kisiyiGuncelle(kisi);
+        }else{
+            kisiyiEkle(kisi);
+        }
     }else{
         bilgiOluştur(kontrol.mesaj, kontrol.durum);
     }
-
-    //console.log(eklenecekKisi);
-
+    //console.log(kisi);
 });
 
 //kisi işlemlerini yap
+// edit - delete
 kisiListesi.addEventListener('click', e =>{
     //console.log(e.target);
     
     if(e.target.classList.contains('btn--delete')){
+        //silme
         //console.log("Silme");
         const silinecekTr = e.target.parentElement.parentElement;
         const silinecekTrMail = e.target.parentElement.previousElementSibling.textContent; //butonun parent'ına gittik sonra ondan bir önceki elemente gittik ve maili bulduk
         rehberdenSil(silinecekTr,silinecekTrMail); //argüman olarak tr 'yi yolladık
     } else if(e.target.classList.contains('btn--edit')){
+        //düzenleme
         //console.log("Düzenleme");
+        document.querySelector('.kaydetGuncelle').value = 'GÜNCELLE'; //Kaydet butonunun text'ini değiştirir.
+        
+        const secilenTr = e.target.parentElement.parentElement;
+        //console.log(secilenTr);
+        const guncellenecekMail = secilenTr.cells[2].textContent;
 
+        
+        ad.value = secilenTr.cells[0].textContent;
+        soyad.value = secilenTr.cells[1].textContent;
+        mail.value = secilenTr.cells[2].textContent;
+
+        secilenSatir = secilenTr;
     } 
 })
+
+function kisiyiGuncelle(kisi){
+    //kisi parametresinde düzenleme için secilen kişinin yeni değerleri vardır
+
+    //dizide güncelleme
+    for (let i = 0; i < tumKisilerDizisi.length; i++) {
+        if(tumKisilerDizisi[i].mail === secilenSatir.cells[2].textContent){
+            tumKisilerDizisi[i] = kisi;
+            break;
+        }
+    }
+    
+    //tabloda güncelleme
+    secilenSatir.cells[0].textContent = kisi.ad;
+    secilenSatir.cells[1].textContent = kisi.soyad;
+    secilenSatir.cells[2].textContent = kisi.mail;
+
+    document.querySelector('.kaydetGuncelle').value = "KAYDET";
+    secilenSatir = undefined;
+
+    //console.log(tumKisilerDizisi);
+}
 
 function rehberdenSil(silinecekTrElementi, silinecekTrMaili){
     silinecekTrElementi.remove();
@@ -55,10 +94,16 @@ function rehberdenSil(silinecekTrElementi, silinecekTrMaili){
             //console.log(tumKisilerDizisi);
         }
     });
+
+    clearInputs();
+    document.querySelector('.kaydetGuncelle').value = "KAYDET";
+    //console.log(tumKisilerDizisi);
 }
     
 // tüm kişiler için dizi
 const tumKisilerDizisi = [];
+let secilenSatir = undefined;
+
 
 function kisiyiEkle(object){
     const olusturulanTrElementi = document.createElement('tr');
